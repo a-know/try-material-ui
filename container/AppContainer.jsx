1452f8bc7c4414d4f83cd7177e52408f0d830ffe
a-know <a-know@hatena.ne.jp>
2020-04-14 14:50:30 +0900

    Implement container

--- container/AppContainer.jsx
+++ container/AppContainer.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { bindActionCreators } from 'redux'
+import { useDispatch, useSelector } from 'react-redux';
 
-import Header from './header/HeaderComponent';
-import Counter from './counter/CounterComponent';
+import Header from '../components/header/HeaderComponent';
+import Counter from '../components/counter/CounterComponent';
+import CounterActions from '../actions/counter';
 
-const AppContainer = () => {
+const useActions = (actions, deps) => {
+  const dispatch = useDispatch();
+  return useMemo(
+    () => {
+      if (Array.isArray(actions)) {
+        return actions.map(a => bindActionCreators(a, dispatch))
+      }
+      return bindActionCreators(actions, dispatch)
+    },
+    deps ? [dispatch, ...deps] : [dispatch]
+  )
+};
+
+const AppContainer = (props) => {
+  const counterActions = useActions(CounterActions);
+  const counter = useSelector(state => state.counter);
+
+  const _counterProps = { counter, counterActions, ...props };
   return (
     <div>
       <Header />
-      <Counter />
+      <Counter {..._counterProps} />
     </div>
   );
 };
+
+export default AppContainer;
