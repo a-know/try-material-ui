ba3e929a7db6b7cebe9b38e57de5de3bd7d20dc8
a-know <a-know@hatena.ne.jp>
2020-04-14 14:48:26 +0900

    Add container and reducer

---
+++ container/AppContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from './header/HeaderComponent';
+import Counter from './counter/CounterComponent';
+
+const AppContainer = () => {
+  return (
+    <div>
+      <Header />
+      <Counter />
+    </div>
+  );
+};
