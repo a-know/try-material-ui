5145803619be68b3254e80345c4f92c50289b7ef
a-know <a-know@hatena.ne.jp>
2020-04-17 09:47:39 +0900

    Add AppBar component

--- components/HeaderComponent.jsx
+++ components/HeaderComponent.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import * as Colors from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    color: Colors.common.white,
+  },
+}));
 
 const HeaderComponent = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <AppBar className={classes.appBar} position="fixed">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            カウンターアプリ
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </header>
   );
 };
+
 
 export default HeaderComponent;
