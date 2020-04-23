1d02d8c81be0827b3758abe66a4a286d57578367
a-know <a.know.dev@gmail.com>
2020-04-23 23:00:02 +0900

    fix counter

--- components/CounterComponent.jsx
+++ components/CounterComponent.jsx
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/AddCircle';
 import RemoveIcon from '@material-ui/icons/RemoveCircle';
+import { useHistory } from 'react-router';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -14,6 +15,11 @@ const useStyles = makeStyles((theme) => ({
 const CounterComponent = (props) => {
   const { counter, counterActions } = props;
   const classes = useStyles();
+  const history = useHistory();
+
+  const handleOnClickToTop = () => {
+    history.push('/');
+  };
 
   return (
     <div style={{ width: '100%' }}>
@@ -38,6 +44,16 @@ const CounterComponent = (props) => {
           onClick={() => counterActions.decrement()}
         >
           減少
+        </Button>
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          fullWidth
+          startIcon={<ExitToApp />}
+          onClick={handleOnClickToTop}
+        >
+          TOPへ戻る
         </Button>
       </div>
     </div>
---
+++ container/counter/CounterContainer.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useMemo } from 'react';
+import { bindActionCreators } from 'redux'
+import { useDispatch, useSelector } from 'react-redux';
+import Counter from '../../components/CounterComponent';
+import { increment, decrement } from '../slices/counter';
+
+const useActions = (actions, deps) => {
+  const dispatch = useDispatch();
+  return useMemo(
+    () => {
+      if (Array.isArray(actions)) {
+        return actions.map(a => bindActionCreators(a, dispatch))
+      }
+      return bindActionCreators(actions, dispatch)
+    },
+    deps ? [dispatch, ...deps] : [dispatch]
+  )
+};
+    
+const CounterContainer = () => {
+  const counterActions = useActions({ increment, decrement });
+  const counter = useSelector(state => state.counter);
+
+  useEffect(() => {
+    console.log('CounterContainer:useEffectによる初回処理');
+  }, []);
+  return (
+    <div>
+      <Counter counter={counter} counterActions={counterActions} />
+    </div>
+  );
+};
+
+export default CounterContainer;
