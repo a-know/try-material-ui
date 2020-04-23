91e75b1b4b3386d716cbe275fd661b17dd1abe71
a-know <a.know.dev@gmail.com>
2020-04-23 09:45:33 +0900

    import index css

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import stores from '../stores';
 import App from '../container/AppContainer';
 
+import './css/main.css';
+
 const render = () => {
   ReactDOM.render(
     <Provider store={stores}>
