67f018e75c71466d9cc98da512c4998acd4f71d5
a-know <a.know.dev@gmail.com>
2020-04-23 09:50:38 +0900

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   context: __dirname,
@@ -16,6 +19,8 @@ module.exports = {
     rules: [
       { test: /\.jsx$/, enforce: 'pre', exclude: /node_modules/, loader: 'eslint-loader' },
       { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+      // cssファイルのload処理
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'], },
     ],
   },
   resolve: {
@@ -34,7 +39,16 @@ module.exports = {
         },
       },
     }),
+    // MiniCssExtractPluginのインスタンスを追記
+    // ここで設定するfilenameは出力するファイル名
+    new MiniCssExtractPlugin({
+      filename: 'css/bundle.css',
+    }),
   ],
+  optimization: {
+    // cssファイルの出力時に、圧縮して最小化する設定
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   devServer: {
     host: '0.0.0.0',
     disableHostCheck: true
