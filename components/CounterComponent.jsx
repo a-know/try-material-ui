4f58f1ec9ade3dbc9a982eabcdd663e212000d14
a-know <a.know.dev@gmail.com>
2020-04-23 09:48:01 +0900

    fix component

--- components/CounterComponent.jsx
+++ components/CounterComponent.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
-
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/AddCircle';
@@ -15,30 +14,32 @@ const useStyles = makeStyles((theme) => ({
 const CounterComponent = (props) => {
   const { counter, counterActions } = props;
   const classes = useStyles();
-  
+
   return (
-    <div>
-      <h2>
-        count={counter.value}
-      </h2>
-      <Button
-        className={classes.button}
-        variant="contained"
-        color="primary"
-        startIcon={<AddIcon />}
-        onClick={() => counterActions.increment()}
-      >
-        増加
-      </Button>
-      <Button
-        className={classes.button}
-        variant="contained"
-        color="secondary"
-        startIcon={<RemoveIcon />}
-        onClick={() => counterActions.decrement()}
-      >
-        減少
-      </Button>
+    <div style={{ width: '100%' }}>
+      <div className="centerTable">
+        <h2>
+          count={counter.value}
+        </h2>
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          startIcon={<AddIcon />}
+          onClick={() => counterActions.increment()}
+        >
+          増加
+        </Button>
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+          startIcon={<RemoveIcon />}
+          onClick={() => counterActions.decrement()}
+        >
+          減少
+        </Button>
+      </div>
     </div>
   );
 };
